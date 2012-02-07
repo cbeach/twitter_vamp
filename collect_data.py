b7340375488fb790776afb77e573f445e25c35c3
Casey Beach <beachc@gmail.com>
2012-02-07 13:34:30 -0800

    Twitter_vamp is consuming user information in a seemingly stable way.  I just got through my first set of 1000 tweets with out interuption.  TODO: add error logging, add tweet and place database insertions, and flesh out the higher level architecture

--- collect_data.py
+++ collect_data.py
@@ -1,4 +1,4 @@
-import pycurl, json, time, sys, commands, pickle, MySQLdb
+import pycurl, json, time, datetime, sys, commands, pickle, MySQLdb
 from sets import Set
 
 STREAM_URL = 'https://stream.twitter.com/1/statuses/sample.json'
@@ -13,9 +13,20 @@ class Client:
     f = open('speed_test', 'a')
     p = open('pickled', 'w')
     tweets = []
-    searching = ["follow_request_sent", "notifications", "following"]
-    
+    user_flags = ["profile_use_background_image", "contributors_enabled", \
+                  "verified", "is_translator", "geo_enabled","protected", \
+                  "default_profile", "profile_background_title", \
+                  "show_all_inline_media", "default_profile_image"]
+    user_columns = ["id", "user_flags", "favourites_count", "friends_count", \
+                    "listed_count", "utc_offset", "statuses_count", "followers_count", \
+                    "profile_image_url_https", "profile_image_url", "profile_background_image_url_https", \
+                    "profile_background_image_url", "profile_sidebar_fill_color", "profile_text_color", \
+                    "profile_link_color", "profile_background_color", "id_str", "created_at", "time_zone", \
+                    "profile_sidebar_border_color", "screen_name", "url", "description", "lang", "place"]
+
+
     def __init__(self):
+
         self.db_init()
         self.create_tables()
         
@@ -38,68 +49,203 @@ class Client:
             sys.exit()
         
     def create_tables(self):
+        #properties in tweet_flags bit field------------------
+        #favorited
+        #truncated
+        #retweeted
         self.db_cursor.execute("CREATE TABLE IF NOT EXISTS \
         tv_tweets(tweet_id BIGINT PRIMARY KEY, created_at INT, text VARCHAR(150), \
-        source VARCHAR(512), favorited BIT, truncated BIT, retweeted BIT, \
+        source VARCHAR(512), tweet_flags BIT(3), \
         retweet_count INT, in_reply_to_user_id INT, in_reply_to_status_id LONG, \
         in_reply_to_screen_name VARCHAR(512), in_reply_to_user_id_str LONG, \
-        in_reply_to_id_str LONG, id_str LONG, geo_type INT, lat FLOAT, lon FLOAT, \
+        in_reply_to_id_str LONG, id_str LONG, geo_type VARCHAR(64), lat FLOAT, lon FLOAT, \
         user_id INT, FOREIGN KEY (user_id) REFERENCES tv_users(user_id), \
         place_id BIGINT, FOREIGN KEY (place_id) REFERENCES tv_places(place_id) )")
 
-#properties in user_flags bit field--------------------
-#profile_use_background_image BIT, \
-#contributors_enabled BIT, \
-#verified BIT, \
-#is_translator BIT, \
-#geo_enabled BIT, \
-#protected BIT, \
-#default_profile BIT, \
-#profile_background_title BIT, \
-#show_all _inline_media BIT, \
-#default_profile_image BIT, \
+        #properties in user_flags bit field--------------------
+        #profile_use_background_image BIT, \
+        #contributors_enabled BIT, \
+        #verified BIT, \
+        #is_translator BIT, \
+        #geo_enabled BIT, \
+        #protected BIT, \
+        #default_profile BIT, \
+        #profile_background_title BIT, \
+        #show_all _inline_media BIT, \
+        #default_profile_image BIT, \
 
 
         self.db_cursor.execute("CREATE TABLE IF NOT EXISTS \
-tv_users(user_id BIGINT PRIMARY KEY, \
-user_flags BIT(10), \
-favorites_count INT, \
-friends_count INT, \
-listed_count INT, \
-utc_offset INT, \
-statuses_count INT, \
-followers_count INT, \
-profile_image_url_https VARCHAR(512), \
-profile_image_url VARCHAR(512), \
-profile_background_image_url_https VARCHAR(512), \
-profile_background_image_url VARCHAR(512), \
-profile_sidebar_fill_color INT, \
-profile_text_color INT, \
-profile_link_color INT, \
-profile_background_color INT, \
-id_str INT, \
-created_at INT, \
-time_zone INT, \
-profile_sidebar_border_color INT, \
-screen_name VARCHAR(512), \
-url VARCHAR(512), \
-description VARCHAR(512), \
-lang VARCHAR(24), \
-place INT, \
-FOREIGN KEY (place) REFERENCES tv_places(place_id) \
-)")  
+        tv_users(user_id BIGINT PRIMARY KEY, user_flags BIT(10), favorites_count INT, \
+        friends_count INT, listed_count INT, utc_offset INT, statuses_count INT, \
+        followers_count INT, profile_image_url_https VARCHAR(512), \
+        profile_image_url VARCHAR(512), profile_background_image_url_https VARCHAR(512), \
+        profile_background_image_url VARCHAR(512), profile_sidebar_fill_color INT, \
+        profile_text_color INT, profile_link_color INT, profile_background_color INT, \
+        id_str INT, created_at INT, time_zone VARCHAR(32), profile_sidebar_border_color INT, \
+        screen_name VARCHAR(512), url VARCHAR(512), description VARCHAR(512), \
+        lang VARCHAR(24), place INT, FOREIGN KEY (place) REFERENCES tv_places(place_id) \
+        )")  
         
         self.db_cursor.execute("CREATE TABLE IF NOT EXISTS \
-tv_places(place_id BIGINT PRIMARY KEY, \
-name VARCHAR(32), \
-url VARCHAR(512), \
-country VARCHAR(32), \
-place_type VARCHAR(32), \
-country_code CHAR(2), \
-full_name VARCHAR(70), \
-bounding_box VARBINARY(128), \
-attributes VARBINARY(1024)\
-)"      )
+        tv_places(place_id BIGINT PRIMARY KEY, name VARCHAR(32), url VARCHAR(512), \
+        country VARCHAR(32), place_type VARCHAR(32), country_code CHAR(2), \
+        full_name VARCHAR(70), bounding_box VARBINARY(128), attributes VARBINARY(1024))")
+
+    #TODO: implement insert function
+    def db_insert(self, content):
+        
+        tweet_insert = "INSERT INTO tv_tweets(tweet_id, created_at, text, source, \
+        favorited, truncated, retweeted, retweet_count, in_reply_to_user_id, \
+        in_reply_to_status_id, in_reply_to_screen_name, in_reply_to_user_id_str, \
+        in_reply_to_id_str, id_str, geo_type, lat, lon, user_id, place_id) VALUES( \
+        "
+
+        user_insert = "INSERT INTO tv_users(user_id, user_flags, favorites_count, \
+        friends_count, listed_count, utc_offset, statuses_count, followers_count, \
+        profile_image_url_https, profile_image_url, profile_background_image_url_https, \
+        profile_background_image_url, profile_sidebar_fill_color, profile_text_color, \
+        profile_link_color, profile_background_color, id_str, created_at, time_zone, \
+        profile_sidebar_border_color, screen_name, url, description, lang, place) \
+        VALUES("
+
+        place_insert = "INSERT INTO tv_places(place_id, name, url, country, place_type, \
+        countrycode, full_name, bounding_box, attributes) VALUES( "
+        
+        #tweet_data = self.format_tweet(content)
+        user_data = self.format_user(content)
+        #place_data = self.format_place(content)
+        if user_data != None: 
+            for i in user_data:
+                user_insert += "'" + str(i) + "'" + ","
+            user_insert = user_insert.rstrip(' ,')
+            user_insert += ');'
+            try: 
+                self.db_cursor.execute(user_insert)
+            except MySQLdb.IntegrityError:
+                pass
+        
+    #----------------------------------------------------
+    #
+    #This is going to be SOOOO ugly :(
+    #
+    #----------------------------------------------------
+    def format_tweet(self,content):
+        values = []
+        flags = 0
+        if content.get("favorited") == 'True':
+            flags << 1
+            flags += 1
+        if content.get("truncated") == 'True':
+            flags << 1
+            flags += 1
+        if content.get("retweeted") == 'True':
+            flags << 1
+            flags += 1
+        try:
+            values.append(content.get("id"))
+            values.append(time.mktime(time.strptime(content.get("created_at"), '%a %b %d %H:%M:%S +0000 %Y')))
+            values.append(content.get("text"))
+            values.append(content.get("source"))
+            values.append(str(flags))
+            values.append(content.get("retweet_count"))
+            values.append(content.get("in_reply_to_user_id"))
+            values.append(content.get("in_reply_to_status_id"))
+            values.append(content.get("in_reply_to_screen_name"))
+            values.append(content.get("in_reply_to_user_id_str"))
+            values.append(content.get("in_reply_to_id_str"))
+            values.append(content.get("geo_type"))
+            values.append(content.get("lat"))
+            values.append(content.get("lon"))
+            values.append(content.get("user_id"))
+            values.append(content.get("place_id"))
+        except TypeError:
+            pass
+
+#TODO: Add a lookup for the place and user foreign keys ------------       
+ 
+    def format_place(self, content):
+        
+        values = []
+        
+        try:
+            values.append(content.get("id"))
+            values.append(content.get("name"))
+            values.append(content.get("url"))
+            values.append(content.get("contry"))
+            values.append(content.get("place_type"))
+            values.append(content.get("country_code"))
+            values.append(content.get("full_name"))
+            values.append(content.get("bounding_box"))
+            values.append(content.get("attributes"))
+        except TypeError:
+            pass
+        return values
+
+    def format_user(self, content):
+        
+        bit_field = 0
+        values = []
+
+        if content.get("user") == None:
+            return None
+
+        for i in self.user_flags:
+            try:
+                if content.get(i) != None:
+                    bit_field += self.__to_bool(content.get(i))
+                bit_field << 1
+            except TypeError:
+                bitfield << 1 
+        
+        for i in self.user_columns:
+            try:
+                if i == "user_flags":
+                    values.append(bit_field)
+                elif i == "created_at":
+                    values.append(time.mktime(time.strptime(content.get("created_at"), '%a %b %d %H:%M:%S +0000 %Y')))
+                elif i == "profile_background_color":
+                    try:
+                        values.append(int(content.get("user").get(i),16))
+                    except (ValueError, AttributeError):
+                        values.append(-1)
+                elif i == "profile_text_color":
+                    try:
+                        values.append(int(content.get("user").get(i),16))
+                    except (ValueError, AttributeError):
+                        values.append(-1)
+                elif i == "profile_sidebar_fill_color":
+                    try:
+                        values.append(int(content.get("user").get(i),16))
+                    except (ValueError, AttributeError):
+                        values.append(-1)
+                elif i == "profile_sidebar_border_color":
+                    try:
+                        values.append(int(content.get("user").get(i),16))
+                    except (ValueError, AttributeError):
+                        values.append(-1)
+                elif i == "description" and content.get("user").get(i) != None:
+                    values.append(MySQLdb.escape_string(content.get("user").get(i).encode('utf-8')))
+                elif i == "profile_link_color":
+                    try:
+                        values.append(int(content.get("user").get(i),16))
+                    except (ValueError, AttributeError):
+                        values.append(-1)
+                elif i == "place" and content.get("user").get(i) == None:
+                    values.append(0)
+                elif i == "utc_offset" and content.get("user").get(i) == None:
+                    values.append(-1)
+                elif content.get("user").get(i) == None:
+                    values.append("NULL")
+                else:
+                    values.append(content.get("user").get(i))
+            except TypeError as e:
+                print "OH NOOOOS"
+                print e
+        return values
+#TODO: Add a lookup for the place foreign key ------------       
+    
+
 
 
     def on_receive(self, data):
@@ -107,16 +253,17 @@ attributes VARBINARY(1024)\
 
         if data.endswith("\r\n") and self.buffer.strip():
             content = json.loads(self.buffer)
+            #print(content.get("user"))
             self.buffer = ""
             
             if self.tweet_count < 1000:
-                self.tweets.append(content)
-                print(self.tweet_count)
+                self.db_insert(content)
             else:
                 print("Done!")
                 self.p.write(pickle.dumps(self.tweets))
                 return 0
             self.tweet_count += 1
+    
     def print_user_attrib(self,data, attrib):
         try:
             if data["user"] != None:
@@ -154,7 +301,12 @@ attributes VARBINARY(1024)\
             except AttributeError:
                 return base_string+"->"+str(type(obj))
         return types_and_keys
-
+    
+    def __to_bool(self, string):
+        if string == 'True':
+            return 1
+        elif string == 'False':
+            return 0
 
 
 
--- types.py
+++ types.py
@@ -1,88 +1,88 @@
 contributors:                           None
 
-#favorited:                              bool
-#truncated:                              bool
-#retweeted:                              bool
+favorited:                              bool
+truncated:                              bool
+retweeted:                              bool
 
-#in_reply_to_user_id:                    int
-#retweet_count:                          int
+in_reply_to_user_id:                    int
+retweet_count:                          int
 
-#id:                                     long
-#in_reply_to_status_id:                  long
+id:                                     long
+in_reply_to_status_id:                  long
 
-#created_at:                             unicode$int
-#text:                                   unicode$varchar
-#in_reply_to_screen_name:                unicode$varchar
-#in_reply_to_user_id_str:                unicode$long
-#source:                                 unicode$varchar
-#in_reply_to_status_id_str:              unicode$long
-#id_str:                                 unicode$long
+created_at:                             unicode$int
+text:                                   unicode$varchar
+in_reply_to_screen_name:                unicode$varchar
+in_reply_to_user_id_str:                unicode$long
+source:                                 unicode$varchar
+in_reply_to_status_id_str:              unicode$long
+id_str:                                 unicode$long
 
 
-#geo:                                    dict
-#    type:                               unicode
-#    coordinates:                        list:float
+geo:                                    dict
+    type:                               unicode
+    coordinates:                        list:float
 
-#coordinates:                            dict
-#    type:                               unicode
-#    coordinates:                        list
+coordinates:                            dict
+    type:                               unicode
+    coordinates:                        list
 
-#entities:                               dict
-#    user_mentions:                      list
-#    hashtags:                           list
-#    urls:                               list
+entities:                               dict
+    user_mentions:                      list
+    hashtags:                           list
+    urls:                               list
 
 place:                                  dict
-#    name:                               unicode$foreign key
-#    url:                                unicode$None            #redundent, can be constructed from country code
-#    country:                            unicode$foreign key
-#    place_type:                         unicode$int             #looks like it's things like city, admin, etc
-#    country_code:                       unicode$unicode
-    full_name:                          unicode$foreign key     #probably redundant
-    id:                                 unicode$int/long        #it looks like it's a long hexidecimal
-    bounding_box:                       dict
-        type                            unicode:int index
-        coordinates                     list:list:float
-    attributes:                         dict:None
+    name:                               unicode$foreign key
+    url:                                unicode$None            #redundent, can be constructed from country code
+    country:                            unicode$foreign key
+    place_type:                         unicode$int             #looks like it's things like city, admin, etc
+    country_code:                       unicode$unicode
+   full_name:                          unicode$foreign key     #probably redundant
+   id:                                 unicode$int/long        #it looks like it's a long hexidecimal
+   bounding_box:                       dict
+       type                            unicode:int index
+       coordinates                     list:list:float
+   attributes:                         dict:None
 
 user: dict
-#    follow_request_sent:                None
-#    following:                          None
-#    notifications:                      None
-#    profile_use_background_image:       bool$bit
-#    contributors_enabled:               bool$bit
-#    verified:                           bool$bit
-#    is_translator:                      bool$bit
-#    geo_enabled:                        bool$bit
-#    protected:                          bool$bit
-#    default_profile:                    bool$bit
-#    profile_background_tile:            bool$bit
-#    show_all_inline_media:              bool$bit
-#    default_profile_image:              bool$bit
-#    id:                                 int$int
-#    favourites_count:                   int$short
-#    friends_count:                      int$short
-#    listed_count:                       int$short
-#    utc_offset:                         int$byte
-#    statuses_count:                     int$short
-#    followers_count:                    int$short
-#    profile_image_url_https:            unicode$unicode         #take out the common prefix
-#    profile_image_url:                  unicode$unicode         #take out the common prefix
-#    profile_background_image_url:       unicode$unicode         #take out common prefix
-#    profile_background_image_url_https: unicode$unicode         #take out common prefix
-#    profile_sidebar_fill_color:         unicode$int
-#    profile_text_color:                 unicode$int
-#    location:                           unicode$foreign key
-#    description:                        unicode$unicode
-#    profile_link_color:                 unicode$int
-#    profile_background_color:           unicode$int
-#    id_str:                             unicode$int             #convert from string
-#    screen_name:                        unicode$unicode
-#    lang:                               unicode$varchar
-#    name:                               unicode$unicode
-#    url:                                unicode
-#    created_at:                         unicode$int
-#    time_zone:                          unicode$int
-#    profile_sidebar_border_color:       unicode$int
+    follow_request_sent:                None
+    following:                          None
+    notifications:                      None
+    profile_use_background_image:       bool$bit
+    contributors_enabled:               bool$bit
+    verified:                           bool$bit
+    is_translator:                      bool$bit
+    geo_enabled:                        bool$bit
+    protected:                          bool$bit
+    default_profile:                    bool$bit
+    profile_background_tile:            bool$bit
+    show_all_inline_media:              bool$bit
+    default_profile_image:              bool$bit
+    id:                                 int$int
+    favourites_count:                   int$short
+    friends_count:                      int$short
+    listed_count:                       int$short
+    utc_offset:                         int$byte
+    statuses_count:                     int$short
+    followers_count:                    int$short
+    profile_image_url_https:            unicode$unicode         #take out the common prefix
+    profile_image_url:                  unicode$unicode         #take out the common prefix
+    profile_background_image_url:       unicode$unicode         #take out common prefix
+    profile_background_image_url_https: unicode$unicode         #take out common prefix
+    profile_sidebar_fill_color:         unicode$int
+    profile_text_color:                 unicode$int
+    location:                           unicode$foreign key
+    description:                        unicode$unicode
+    profile_link_color:                 unicode$int
+    profile_background_color:           unicode$int
+    id_str:                             unicode$int             #convert from string
+    screen_name:                        unicode$unicode
+    lang:                               unicode$varchar
+    name:                               unicode$unicode
+    url:                                unicode
+    created_at:                         unicode$int
+    time_zone:                          unicode$int
+    profile_sidebar_border_color:       unicode$int
 
 
