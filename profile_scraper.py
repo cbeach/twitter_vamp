484200fdcea29ae8caa63c4119b976479b1ff0a5
Casey Beach <beachc@gmail.com>
2012-05-23 12:48:23 -0700

    I updated profile scraper to use redis, so users can easily be added.

--- profile_scraper.py
+++ profile_scraper.py
@@ -1,58 +1,48 @@
-import tweepy, time, config, json
+import tweepy, time, config, json, redis, bz2
 
 query_rate = 150 #queries / hr
 
-# == OAuth Authentication ==
-#
-# This mode of authentication is the new preferred way
-# of authenticating with Twitter.
-
-# The consumer keys can be found on your application's Details
-# page located at https://dev.twitter.com/apps (under "OAuth settings")
 consumer_key= config.consumer_key
 consumer_secret=config.consumer_secret
-
-# The access tokens can be found on your applications's Details
-# page located at https://dev.twitter.com/apps (located 
-# under "Your access token")
 access_token=config.access_token
 access_token_secret=config.access_token_secret
 
 auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
 auth.set_access_token(access_token, access_token_secret)
-
 api = tweepy.API(auth)
 
-# If the authentication was successful, you should
-# see the name of the account print out
 print "logged in as %s" % api.me().name
-
-# If the application settings are set for "Read and Write" then
-# this line should tweet out the message to your account's 
-# timeline. The "Read and Write" setting is on https://dev.twitter.com/apps
-#api.update_status('Tweepy works ^_^')
-
-tracked_user = "Nike"
 
 
 cur = tweepy.Cursor(api.followers,id=tracked_user)
+redis_server = redis.Redis("localhost") 
 
 followers_collected = 0
 
 user_list = set()
 
-for user in cur.items():
-    user_list.add((user.screen_name, user.id))
-    followers_collected += 1
-    time.sleep(3600/query_rate) #3600=seconds in an hour
-    if followers_collected % 10000 == 0:
-        f = open("follower_lists/followers-%s-%s" % (tracked_user, time.strftime('%Y-%m-%d-%H-%M')), 'w')
-        temp = [i for i in user_list]
-        json.dump(temp, f) 
-        
+#query the redis db for users.
+#if there's a user, start pulling users.
+#if there isn't sleep for 15 minutes
+#
+
+while(True):
+    
+    if len(redis_server.lrange("followed_users",0,-1) < 1):
+        sleep(900)  #15 minutes in seconds 
+        continue
+
+    tracked_user = redis_server.lpop("followed_users")
+    for user in cur.items():
+        user_list.add((user.screen_name, user.id))
+        followers_collected += 1
+        time.sleep(3600/query_rate) #3600=seconds in an hour
+        if followers_collected % 50000 == 0:
+            f = bz2.BZ2File("follower_lists/%s.json.bz2" % (tracked_user), 'w')
+            temp = [i for i in user_list]
+            json.dump(temp, f) 
 
         
-    print((user.screen_name, user.id))
 
 
 
