481020048c52b382e4d8b9d54629095773323790
Casey Beach <mcsmash@Jane.(none)>
2012-07-14 13:11:39 -0700

    I reoganized things a little.  I bound the language detection netbeans projects to local directories so I track them in this repository.

---
+++ core/config.py
@@ -0,0 +1,14 @@
+STREAM_URL = 'https://stream.twitter.com/1/statuses/sample.json'
+USER = "OverlordMcSmash"
+PASS = "Omicron2"
+
+RABBITMQ_HOST = 'localhost'
+RAW_EXCHANGE = 'direct.raw'
+
+
+#twitter oauth keys
+consumer_key="ZBMgwhGMLT50VQSU1mQzA"
+consumer_secret="SjfwieNnvcMiw5EQbI6sqwfTjAMPpVUDArVDBSHMmgE"
+
+access_token="553225618-6uCqFqhENovcTqMWaehk0gGdqoC8qA4obEW12DU1"
+access_token_secret="IW5iphTohvQVKGovv6cuk0lNanduz5QIZxDudqFY"
---
+++ manage.py
@@ -0,0 +1,27 @@
+import json, time, datetime, sys, commands, pickle, config, pika, bz2, tweepy
+import subprocess
+from core.fetcher import * 
+from core.archiver import * 
+
+consumer_key= config.consumer_key
+consumer_secret=config.consumer_secret
+access_token=config.access_token
+access_token_secret=config.access_token_secret
+
+auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+auth.set_access_token(access_token, access_token_secret)
+api = tweepy.API(auth, parser=RawJsonParser())
+
+print "logged in"
+
+while(True):
+    archiver_sp = subprocess.Popen(['python', 'core/archiver.py', 'data/raw'])
+    language = subprocess.Popen()
+    try:
+        watcher = StreamListener()
+        streamer = tweepy.Stream(auth=auth, listener=watcher)
+        streamer.sample()
+    except Exception as e:
+        print("Error Encountered: %s" % e)
+        time.sleep(30)  
+
