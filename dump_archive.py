d8bede63395dde6d38114bdda1e13f8b89961b37
Casey Beach <mcsmash@Jane.(none)>
2012-07-18 23:34:37 -0700

    Fixed an AIB, and the short by languages script.  Sorting now only spawns one language detector.

--- dump_archive.py
+++ dump_archive.py
@@ -1,8 +1,8 @@
 import os, json, redis, bz2, config, time, gc, logging
 
 
-logging.basicConfig(filename='./logs/archive_replay.log')
-log = logging.getLogger(__name__)
+#logging.basicConfig(filename='./logs/archive_replay.log')
+#log = logging.getLogger(__name__)
 
 redis_server = redis.StrictRedis(config.REDIS_SERVER)
 
@@ -20,7 +20,7 @@ def dump_data(f, position=None):
         dump_data.buff += data
         if data.endswith("\r\n") and dump_data.buff.strip():
             redis_server.rpush('lang', dump_data.buff)
-            redis_server.rpush('entities', dump_data.buff)
+#            redis_server.rpush('entities', dump_data.buff)
             dump_data.buff = ''
         if redis_server.llen('lang') > 50000:
             wait_for_room()
@@ -36,8 +36,9 @@ except IOError:
 with open('data/sorted_archives', 'a') as p:
     for path in archives:
         if path in processed:
-            print 'Processing %s' % path
+            print 'Skipping %s' % i
             continue
+        print 'Processing %s' % path
         try:
             f = bz2.BZ2File(os.path.join('data/raw/',path),'r')
             dump_data(f)
--- sort_by_language.sh
+++ sort_by_language.sh
@@ -3,12 +3,6 @@
 rm data/sorted_archive
 rm data/sorted_archives
 
-python dump_archive.py &
 python nlp/sort_by_lang.py * &
-
-
 java -jar nlp/DetectLang/dist/DetectLang.jar &
-java -jar nlp/DetectLang/dist/DetectLang.jar &
-java -jar nlp/DetectLang/dist/DetectLang.jar &
-java -jar nlp/DetectLang/dist/DetectLang.jar &
-
+python dump_archive.py &
